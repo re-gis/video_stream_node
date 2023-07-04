c200542faf5b83fb7c7248026e56a8f7c2146076
D Regis <irumvaregisdmc@gmail.com>
2023-07-05 00:57:58 +0200

    Accessing the camera

---
+++ public/main.js
@@ -0,0 +1,10 @@
+const video = document.getElementById("camera-stream");
+
+navigator.mediaDevices
+  .getUserMedia({ video: true })
+  .then((stream) => {
+    video.srcObject = stream;
+  })
+  .catch((error) => {
+    console.error("Error accessing camera:", error);
+  });
